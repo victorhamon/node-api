5542b59dd39c72ac72a7a55afa5c2094f3ab9558
victorhamon <victorhribeiro@gmail.com>
2019-09-22 19:54:28 -0300

    sync commit

---
+++ src/app.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const config = require('./config');
+
+const app = express();
+const router = express.Router();
+
+//Database connection
+mongoose.connect(config.connectionString, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+});
+mongoose.set('useCreateIndex', true);
+
+//Load models
+const Product = require('./models/product');
+const Customer = require('./models/customer');
+const Order = require('./models/order');
+
+//Load routes
+const indexRoute = require('./routes/index-route');
+const productRoute = require('./routes/product-route');
+const customerRoute = require('./routes/customer-route');
+const orderRoute = require('./routes/order-route');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/', indexRoute);
+app.use('/products', productRoute);
+app.use('/customers', customerRoute);
+app.use('/orders', orderRoute);
+
+module.exports = app;
