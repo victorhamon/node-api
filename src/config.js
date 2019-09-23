84114f2ce01e75bd8224faccc104409d61db5568
victorhamon <victorhribeiro@gmail.com>
2019-09-23 13:16:06 -0300

    mongo db secure issue handled

--- src/config.js
+++ src/config.js
@@ -2,6 +2,6 @@ global.SALT_KEY = 'f5b99242-6504-4ca3-90f2-05e78e5761ef';
 global.EMAIL_TMPL = 'Olá, <strong>{0}</strong>, seja bem vindo ao Node Store!';
 
 module.exports = {
-    connectionString: 'mongodb+srv://victor-hamon:naufrago@cluster0-6zscd.mongodb.net/node-str?retryWrites=true&w=majority',
+    connectionString: `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0-6zscd.mongodb.net/node-str?retryWrites=true&w=majority`,
     containerConnectionString: 'TBD'
 }
