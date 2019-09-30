64d14ebd76cf93571d3813319c1cf74cb8f4d91b
victorhamon <victorhribeiro@gmail.com>
2019-09-30 10:47:01 -0300

    auth service added

--- src/controllers/customer-controller.js
+++ src/controllers/customer-controller.js
@@ -6,6 +6,7 @@ const md5 = require('md5');
 const emailService = require('../services/email-service');
 const smsService = require('../services/sms-service');
 const PNNormalizeService = require('../services/phone-number-normalize-service');
+const authService = require('../services/auth-service');
 
 exports.post = async (req, res, next) => {
     let contract = new ValidationContract();
@@ -34,7 +35,7 @@ exports.post = async (req, res, next) => {
             req.body.name)
             );
 
-            var phoneNumber = await PNNormalizeService.normalize(req.body.phoneNumber, req.body.country);
+        var phoneNumber = await PNNormalizeService.normalize(req.body.phoneNumber, req.body.country);
 
         smsService.send(
             phoneNumber,
@@ -43,6 +44,39 @@ exports.post = async (req, res, next) => {
 
         res.status(201).send({
             message: 'Cliente cadastrado com sucesso.'
+        });
+    } catch (e) {
+        res.status(500).send({
+            message: 'Falha ao processar a requisição.'
+        });
+    }
+};
+
+exports.authenticate = async (req, res, next) => {
+    try {
+        const customer = await repository.authenticate({
+            email: req.body.email,
+            password: md5(req.body.password + global.SALT_KEY)
+        });
+
+        if (!customer) {
+            res.status(404).send({
+                message: 'Usuário ou senha inválidos.'
+            });
+            return;
+        }
+
+        const token = await authService.generateToken({
+            email: customer.email,
+            name: customer.name
+        });
+
+        res.status(201).send({
+            token: token,
+            data: {
+                email: customer.email,
+                name: customer.name
+            }
         });
     } catch (e) {
         res.status(500).send({
