2ce7339299cf14f741aa71c525c197b1b5c7e2a7
victorhamon <victorhribeiro@gmail.com>
2019-09-30 14:53:14 -0300

    cors enabled

--- src/app.js
+++ src/app.js
@@ -27,8 +27,22 @@ const productRoute = require('./routes/product-route');
 const customerRoute = require('./routes/customer-route');
 const orderRoute = require('./routes/order-route');
 
-app.use(bodyParser.json({ limit: '10mb', extended: true }));
-app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
+app.use(bodyParser.json({
+    limit: '10mb',
+    extended: true
+}));
+app.use(bodyParser.urlencoded({
+    limit: '10mb',
+    extended: true
+}));
+
+//Enable Cors
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-access-token');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    next();
+});
 
 app.use('/', indexRoute);
 app.use('/products', productRoute);
